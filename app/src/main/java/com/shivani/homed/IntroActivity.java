63cf83b9444528d194cffb8531a18f44dc547184
Shivani Sorte <sssorte1428@gmail.com>
2022-02-03 17:54:03 +0530

    app logo, lottie animation

---
+++ app/src/main/java/com/shivani/homed/IntroActivity.java
@@ -0,0 +1,23 @@
+package com.shivani.homed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        lottieAnimationView = findViewById(R.id.lottie);
+        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
+
+
+    }
+}
