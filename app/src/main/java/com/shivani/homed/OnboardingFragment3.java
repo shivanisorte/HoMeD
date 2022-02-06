1c91f3e54bb612113b1245e0c064d01a7817ec36
Shivani Sorte <sssorte1428@gmail.com>
2022-02-06 21:59:04 +0530

    added next button

--- app/src/main/java/com/shivani/homed/OnboardingFragment3.java
+++ app/src/main/java/com/shivani/homed/OnboardingFragment3.java
@@ -1,5 +1,7 @@
 package com.shivani.homed;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +11,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import org.jetbrains.annotations.NotNull;
 
 public class OnboardingFragment3 extends Fragment {
+    FloatingActionButton fab;
+
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_onboarding3, container, false);
+
+        fab = root.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return root;
     }
